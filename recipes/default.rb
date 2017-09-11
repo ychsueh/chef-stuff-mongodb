bd579c2f7a3cefe6e890246216c677f266e9b4eb
Yu-Chen Hsueh <ychsueh@gmail.com>
2017-09-11 09:57:08 +0000

    first commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,28 @@
-#
-# Cookbook:: learn_chef_mongodb
+#Cookbook Name:: learn_chef_mongodb
 # Recipe:: default
 #
-# Copyright:: 2017, The Authors, All Rights Reserved.
+#
+
+# Create a /etc/yum.repos.d/mongodb.repo file to hold the configuration
+baseurl = "http://downloads-distro.mongodb.org/repo/redhat/os/x86_64/"
+
+if node["kernel"]["machine"] != "x86_64"
+  baseurl = "http://downloads-distro.mongodb.org/repo/redhat/os/i686/"
+end
+
+template "/etc/yum.repos.d/mongodb.repo"  do
+  variables(
+    :baseurl => baseurl
+  )
+end
+
+# Install the MongoDB packages and associated tools.
+package "mongodb-org" do
+  action :install
+end
+
+# Start MongoDB.
+# ensure that MongoDB will start following a system reboot
+service "mongod" do
+  action [:start, :enable]
+end
